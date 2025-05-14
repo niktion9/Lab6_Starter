da6385bfbc269b19198ded3a8a7e8bf9ff4c1955
NikithaM <150311091+niktion9@users.noreply.github.com>
2025-05-14 06:27:14 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,7 +6,49 @@ window.addEventListener("DOMContentLoaded", init);
 // Starts the program, all function calls trace back here
 function init() {
 	// Get the recipes from localStorage
+	const localData = localStorage.getItem('recipes');
 	let recipes = getRecipesFromStorage();
+	if (localData === null) {
+		recipes = [
+			{
+				imgSrc: "./assets/images/1_spooky-ghost-cookies.jpeg",
+				imgAlt: "Spooky Ghost Cookies",
+				titleLnk: "https://www.delish.com/holiday-recipes/halloween/a28637917/ghost-cookies-recipe/",
+				titleTxt: "Spooky Ghost Cookies",
+				organization: "Delish.com",
+				rating: 5,
+				numRatings: 1,
+				lengthTime: "2 hr",
+				ingredients: "Light corn syrup, almond, black food coloring, powdered sugar,"
+			},
+			{
+				imgSrc: "./assets/images/2_frightfully-easy-ghost-cookies.jpeg",
+				imgAlt: "Ghost cookies in pumpkin bowl",
+				titleLnk: "https://www.pillsbury.com/recipes/frightfully-easy-ghost-cookies/bed2af7e-59a0-4b68-be25-1dcaeca66254",
+				titleTxt: "Frightfully Easy Ghost Cookies",
+				organization: "Pillsbury",
+				rating: 4,
+				numRatings: 90,
+				lengthTime: "30 min",
+				ingredients: "Peanut butter filled, chocolate chips, candy coating"
+			},
+			{
+				imgSrc: "./assets/images/3_ingredient-ghost-halloween-cookies.jpeg",
+				imgAlt: "Ghost cookies in metal tin",
+				titleLnk: "https://butterwithasideofbread.com/easy-ghost-halloween-cookies/",
+				titleTxt: "3 Ingredient Easy Ghost Halloween Cookies",
+				organization: "Butter with a Side of Bread",
+				rating: 0,
+				numRatings: 0,
+				lengthTime: "10 min",
+				ingredients: "White almond bark, mini chocolate chips"
+			}
+		];
+		saveRecipesToStorage(recipes); 
+	}
+	else{
+		recipes = JSON.parse(localData);
+	}
 	// Add each recipe to the <main> element
 	addRecipesToDocument(recipes);
 	// Add the event listeners to the form elements
